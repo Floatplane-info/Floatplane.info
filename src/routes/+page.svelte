a2f4ba696e09b08a863893a1be5b5d9ea04fc895
ajgeiss0702 <aj@ajg0702.us>
2026-04-19 20:06:29 -0700

    add front page styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import * as InputGroup from "$lib/components/ui/input-group/index.js";
+    import SearchIcon from "@lucide/svelte/icons/search";
+</script>
+
+<div class="limit mx-auto flex items-center justify-center h-screen">
+    <div class="w-full text-center">
+        <h1 class="text-6xl mb-6">Floatplane Search</h1>
+
+        <form action="/search" method="get" class="">
+            <InputGroup.Root>
+                <InputGroup.Input class="text-xl" type="search" name="q" placeholder="Search across floatplane..." />
+                <InputGroup.Addon>
+                    <SearchIcon />
+                </InputGroup.Addon>
+                <InputGroup.Addon align="inline-end">
+                    <InputGroup.Button variant="secondary" class="cursor-pointer">Search</InputGroup.Button>
+                </InputGroup.Addon>
+            </InputGroup.Root>
+        </form>
+    </div>
+</div>

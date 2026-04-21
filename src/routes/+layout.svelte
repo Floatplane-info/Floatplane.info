4036b61319c4f7f833fda186208745156aa331d1
ajgeiss0702 <aj@ajg0702.us>
2026-04-21 16:41:53 -0700

    add nprogress loading bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
 	import './layout.css';
+    import 'nprogress/nprogress.css';
+
+
     import { ModeWatcher } from "mode-watcher";
 	import favicon from '$lib/assets/favicon.svg';
     import * as ToolTip from "$lib/components/ui/tooltip";
+    import {afterNavigate, beforeNavigate} from "$app/navigation";
+    import {navigating} from "$app/state";
+    import NProgress from 'nprogress';
 
 	let { children } = $props();
+
+    NProgress.configure({
+        minimum: 0.16
+    });
+
+    let progressTimeout: number;
+
+    beforeNavigate(n => {
+        if (progressTimeout) clearTimeout(progressTimeout);
+        progressTimeout = setTimeout(() => {
+            if (navigating.type) {
+                NProgress.start();
+            }
+        }, 150) as unknown as number;
+    })
+
+    afterNavigate(() => {
+        if (progressTimeout) clearTimeout(progressTimeout);
+        NProgress.done();
+    })
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>

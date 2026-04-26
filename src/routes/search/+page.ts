8b2034f0c64a5b24ae5848c44967943141b7c588
ajgeiss0702 <aj@ajg0702.us>
2026-04-26 10:44:20 -0700

    auto retry search fetch in case of client errors

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -1,9 +1,13 @@
 import type { PageLoad } from "./$types";
 import type {SearchApiResponse} from "../api/search/+server.ts";
+import {retry} from "$lib/utils";
 
 
 export const load: PageLoad = async ({url, fetch}) => {
     const searchUrl = new URL(url);
     searchUrl.pathname = "/api/search";
-    return await fetch(searchUrl).then(r => r.json() as Promise<SearchApiResponse>);
+    return await retry(() =>
+        fetch(searchUrl)
+            .then(r => r.json() as Promise<SearchApiResponse>)
+    );
 }
